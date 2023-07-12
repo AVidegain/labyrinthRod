99608757f064a4c11c9ac094ad071d1befec301e
AVidegain <alvarovidegain4@gmail.com>
2023-07-12 23:04:38 +0200

    refactor(LabyrinthState): define rod status as its own class to check visited states. Fix algorithm bugs

--- LabyrinthSearch.py
+++ LabyrinthSearch.py
@@ -1,3 +1,4 @@
+from collections import deque
 from dataclasses import dataclass, replace
 from enum import Enum
 
@@ -18,6 +19,14 @@ class Coordinates:
 
 
 @dataclass
+class RodState:
+    """Dataclass representing the rod position and orientation."""
+
+    pos: Coordinates
+    orientation: Orientation
+
+
+@dataclass
 class LabyrinthState:
     """Dataclass representing the entire system. `n_rows` and `n_cols` indicate the
     labyrinth size, `walls` indicate where there are obstacles in the labyrinth,
@@ -27,13 +36,12 @@ class LabyrinthState:
 
     n_rows: int
     n_cols: int
-    walls: list[Coordinates]
-    rod_pos: Coordinates
-    rod_orientation: Orientation
+    walls: tuple[Coordinates]
+    rod: RodState
     steps: int
 
 
-def val_lab(raw_labyrinth: list[list[str]]) -> bool:
+def validate_lab(raw_labyrinth: list[list[str]]) -> bool:
     """Checks that the input represents a valid labyrinth.
 
     Args:
@@ -80,6 +88,43 @@ def val_lab(raw_labyrinth: list[list[str]]) -> bool:
     return True
 
 
+def labyrinth_parser(raw_labyrinth: list[list[str]]) -> LabyrinthState:
+    """Parses the input into a `labyrinthState`.
+
+    Args:
+        raw_labyrinth (list[list[str]]): Lists describing the input using '.' and '#'.
+
+    Raises:
+        ValueError: Raises exception if the input is not valid.
+
+    Returns:
+        LabyrinthState
+    """
+
+    # Check that the input is valid.
+    if not validate_lab(raw_labyrinth):
+        raise ValueError("Invalid input")
+
+    walls = []
+
+    for y, row in enumerate(raw_labyrinth):
+        for x, element in enumerate(row):
+            if element == "#":
+                walls.append(Coordinates(x=x, y=y))
+
+    n_rows = len(raw_labyrinth)
+    n_cols = len(raw_labyrinth[0])
+    rod = RodState(pos=Coordinates(x=1, y=0), orientation=Orientation.HORIZONTAL)
+
+    return LabyrinthState(
+        n_rows=n_rows,
+        n_cols=n_cols,
+        walls=tuple(walls),
+        rod=rod,
+        steps=0,
+    )
+
+
 def get_valid_moves(state: LabyrinthState) -> list[LabyrinthState]:
     """Get all the states that can be reached from a given state.
 
@@ -91,63 +136,80 @@ def get_valid_moves(state: LabyrinthState) -> list[LabyrinthState]:
     """
     valid_moves = []
 
-    if state.rod_orientation is Orientation.HORIZONTAL:
+    if state.rod.orientation is Orientation.HORIZONTAL:
         # Move to the right.
-        right_spot = Coordinates(state.rod_pos.x + 2, state.rod_pos.y)
-        if right_spot.x < state.n_cols - 1 and right_spot not in state.walls:
+        right_spot = Coordinates(state.rod.pos.x + 2, state.rod.pos.y)
+        if right_spot.x < state.n_cols and right_spot not in state.walls:
+            rod = RodState(
+                pos=Coordinates(state.rod.pos.x + 1, state.rod.pos.y),
+                orientation=state.rod.orientation,
+            )
             move = replace(
                 state,
-                rod_pos=Coordinates(state.rod_pos.x + 1, state.rod_pos.y),
+                rod=rod,
                 steps=state.steps + 1,
             )
             valid_moves.append(move)
 
         # Move to the left.
-        left_spot = Coordinates(state.rod_pos.x - 2, state.rod_pos.y)
-        if left_spot.x > 0 and left_spot not in state.walls:
+        left_spot = Coordinates(state.rod.pos.x - 2, state.rod.pos.y)
+        if left_spot.x >= 0 and left_spot not in state.walls:
+            rod = RodState(
+                pos=Coordinates(state.rod.pos.x - 1, state.rod.pos.y),
+                orientation=state.rod.orientation,
+            )
             move = replace(
                 state,
-                rod_pos=Coordinates(state.rod_pos.x - 1, state.rod_pos.y),
+                rod=rod,
                 steps=state.steps + 1,
             )
             valid_moves.append(move)
 
         # Move up.
         above_spots = [
-            Coordinates(state.rod_pos.x + i, state.rod_pos.y - 1) for i in range(-1, 2)
+            Coordinates(state.rod.pos.x + i, state.rod.pos.y - 1) for i in range(-1, 2)
         ]
-        if all(spot.y > 0 and spot not in state.walls for spot in above_spots):
+        if all(spot.y >= 0 and spot not in state.walls for spot in above_spots):
+            rod = RodState(
+                pos=Coordinates(state.rod.pos.x, state.rod.pos.y - 1),
+                orientation=state.rod.orientation,
+            )
             move = replace(
                 state,
-                rod_pos=Coordinates(state.rod_pos.x, state.rod_pos.y - 1),
+                rod=rod,
                 steps=state.steps + 1,
             )
             valid_moves.append(move)
 
         # Move down.
         below_spots = [
-            Coordinates(state.rod_pos.x + i, state.rod_pos.y + 1) for i in range(-1, 2)
+            Coordinates(state.rod.pos.x + i, state.rod.pos.y + 1) for i in range(-1, 2)
         ]
+
         if all(
-            spot.y < state.n_rows - 1 and spot not in state.walls
-            for spot in below_spots
+            spot.y < state.n_rows and spot not in state.walls for spot in below_spots
         ):
+            rod = RodState(
+                pos=Coordinates(state.rod.pos.x, state.rod.pos.y + 1),
+                orientation=state.rod.orientation,
+            )
             move = replace(
                 state,
-                rod_pos=Coordinates(state.rod_pos.x, state.rod_pos.y + 1),
+                rod=rod,
                 steps=state.steps + 1,
             )
             valid_moves.append(move)
 
         # Rotate.
         if (
-            all(spot.y > 0 for spot in above_spots)
-            and all(spot.y < state.n_rows - 1 for spot in below_spots)
+            all(spot.y >= 0 for spot in above_spots)
+            and all(spot.y < state.n_rows for spot in below_spots)
             and all(spot not in state.walls for spot in above_spots + below_spots)
         ):
+            rod = RodState(pos=state.rod.pos, orientation=Orientation.VERTICAL)
             move = replace(
                 state,
-                rod_orientation=Orientation.VERTICAL,
+                rod=rod,
                 steps=state.steps + 1,
             )
             valid_moves.append(move)
@@ -156,60 +218,76 @@ def get_valid_moves(state: LabyrinthState) -> list[LabyrinthState]:
     else:
         # Move to the right.
         right_spots = [
-            Coordinates(state.rod_pos.x + 1, state.rod_pos.y + i) for i in range(-1, 2)
+            Coordinates(state.rod.pos.x + 1, state.rod.pos.y + i) for i in range(-1, 2)
         ]
         if all(
-            spot.x < state.n_cols - 1 and spot not in state.walls
-            for spot in right_spots
+            spot.x < state.n_cols and spot not in state.walls for spot in right_spots
         ):
+            rod = RodState(
+                pos=Coordinates(state.rod.pos.x + 1, state.rod.pos.y),
+                orientation=state.rod.orientation,
+            )
             move = replace(
                 state,
-                rod_pos=Coordinates(state.rod_pos.x + 1, state.rod_pos.y),
+                rod=rod,
                 steps=state.steps + 1,
             )
             valid_moves.append(move)
 
         # Move to the left.
         left_spots = [
-            Coordinates(state.rod_pos.x - 1, state.rod_pos.y + i) for i in range(-1, 2)
+            Coordinates(state.rod.pos.x - 1, state.rod.pos.y + i) for i in range(-1, 2)
         ]
-        if all(spot.x > 0 and spot not in state.walls for spot in left_spots):
+        if all(spot.x >= 0 and spot not in state.walls for spot in left_spots):
+            rod = RodState(
+                pos=Coordinates(state.rod.pos.x - 1, state.rod.pos.y),
+                orientation=state.rod.orientation,
+            )
             move = replace(
                 state,
-                rod_pos=Coordinates(state.rod_pos.x - 1, state.rod_pos.y),
+                rod=rod,
                 steps=state.steps + 1,
             )
             valid_moves.append(move)
 
         # Move up.
-        above_spot = Coordinates(state.rod_pos.x, state.rod_pos.y - 2)
-        if above_spot.y > 0 and above_spot not in state.walls:
+        above_spot = Coordinates(state.rod.pos.x, state.rod.pos.y - 2)
+        if above_spot.y >= 0 and above_spot not in state.walls:
+            rod = RodState(
+                pos=Coordinates(state.rod.pos.x, state.rod.pos.y - 1),
+                orientation=state.rod.orientation,
+            )
             move = replace(
                 state,
-                rod_pos=Coordinates(state.rod_pos.x, state.rod_pos.y - 1),
+                rod=rod,
                 steps=state.steps + 1,
             )
             valid_moves.append(move)
 
         # Move down.
-        below_spot = Coordinates(state.rod_pos.x, state.rod_pos.y + 2)
-        if below_spot.y < state.n_rows - 1 and below_spot not in state.walls:
+        below_spot = Coordinates(state.rod.pos.x, state.rod.pos.y + 2)
+        if below_spot.y < state.n_rows and below_spot not in state.walls:
+            rod = RodState(
+                pos=Coordinates(state.rod.pos.x, state.rod.pos.y + 1),
+                orientation=state.rod.orientation,
+            )
             move = replace(
                 state,
-                rod_pos=Coordinates(state.rod_pos.x, state.rod_pos.y + 1),
+                rod=rod,
                 steps=state.steps + 1,
             )
             valid_moves.append(move)
 
         # Rotate.
         if (
-            all(spot.y > 0 for spot in right_spots)
-            and all(spot.y < state.n_rows - 1 for spot in left_spots)
+            all(spot.y >= 0 for spot in right_spots)
+            and all(spot.y < state.n_rows for spot in left_spots)
             and all(spot not in state.walls for spot in right_spots + left_spots)
         ):
+            rod = RodState(pos=state.rod.pos, orientation=Orientation.HORIZONTAL)
             move = replace(
                 state,
-                rod_orientation=Orientation.HORIZONTAL,
+                rod=rod,
                 steps=state.steps + 1,
             )
             valid_moves.append(move)
@@ -229,70 +307,32 @@ def solve_labyrinth(initial_state: LabyrinthState) -> int:
     """
 
     # Visited states and states to explore.
-    visited = set()
-    states_to_explore = {initial_state}
+    visited = []
+    states_to_explore = deque([initial_state])
 
     while states_to_explore:
-        state = states_to_explore.pop()
+        state = states_to_explore.popleft()
 
-        if state in visited:
+        if state.rod in visited:
             continue
 
-        visited.add(state)
-
-        # Reached the bottom-right corner. There is no need to check for orientation.
-        if (
-            state.rod_pos.x == state.n_rows - 1 and state.rod_pos.y == state.n_cols - 2
-        ) or (
-            state.rod_pos.x == state.n_rows - 2 and state.rod_pos.y == state.n_cols - 1
-        ):
-            return state.steps
+        visited.append(state.rod)
 
         # Check for valid moves
         valid_moves = get_valid_moves(state)
 
         # Add valid moves to current states
-        states_to_explore.update(valid_moves)
+        states_to_explore.extend(valid_moves)
+
+        # Reached the bottom-right corner. There is no need to check for orientation.
+        if (
+            state.rod.pos.x == state.n_cols - 1 and state.rod.pos.y == state.n_rows - 2
+        ) or (
+            state.rod.pos.x == state.n_cols - 2 and state.rod.pos.y == state.n_rows - 1
+        ):
+            return state.steps
 
     return -1
-
-
-def labyrinth_parser(raw_labyrinth: list[list[str]]) -> LabyrinthState:
-    """Parses the input into a `labyrinthState`.
-
-    Args:
-        raw_labyrinth (list[list[str]]): Lists describing the input using '.' and '#'.
-
-    Raises:
-        ValueError: Raises exception if the input is not valid.
-
-    Returns:
-        LabyrinthState
-    """
-
-    # Check that the input is valid.
-    if not val_lab(raw_labyrinth):
-        raise ValueError("Invalid input")
-
-    walls = []
-
-    for x, row in enumerate(raw_labyrinth):
-        for y, element in enumerate(row):
-            if element == "#":
-                walls.append(Coordinates(x=x, y=y))
-
-    n_rows = len(raw_labyrinth)
-    n_cols = len(raw_labyrinth[0])
-    rod_pos = Coordinates(x=1, y=0)
-
-    return LabyrinthState(
-        n_rows=n_rows,
-        n_cols=n_cols,
-        walls=walls,
-        rod_pos=rod_pos,
-        rod_orientation=Orientation.HORIZONTAL,
-        steps=0,
-    )
 
 
 def min_dist_lab(raw_labyrinth: list[list[str]]) -> int:
